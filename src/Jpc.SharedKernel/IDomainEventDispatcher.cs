c102638cbd5c182d7cf1d00aeb10c27f34b543aa
Jeffrey <Jeffrey@DESKTOP-L6EVMTT>
2024-04-13 22:27:29 +0200

    Add support for generic etities

--- src/Jpc.SharedKernel/IDomainEventDispatcher.cs
+++ src/Jpc.SharedKernel/IDomainEventDispatcher.cs
@@ -1,4 +1,5 @@
-﻿namespace Jpc.SharedKernel;
+﻿
+namespace Jpc.SharedKernel;
 
 /// <summary>
 /// A simple interface for sending domain events. Can use MediatR or any other implementation.
@@ -6,4 +7,5 @@
 public interface IDomainEventDispatcher
 {
     Task DispatchAndClearEvents(IEnumerable<HasDomainEventsBase> entitiesWithEvents);
+    Task DispatchAndClearEvents<TId>(IEnumerable<EntityBase<TId>> entitiesWithEvents) where TId : struct, IEquatable<TId>;
 }
